17c282c398b2a9a41fc29d84e8481b5cc959fc72
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-06-02 22:19:33 -0400

    Write camera rotation & W change, change "parseCSV" to "Data"

---
+++ Camera/Camera.java
@@ -0,0 +1,37 @@
+boolean holdingW = false;
+boolean holdingShift = false;
+// set to false every draw()
+
+void mouseDragged() {
+  double WRate = 0.01;
+  double XYRate = 0.01;
+
+  if (holdingW && dimension >= 4) {
+    currentW *= (pmouseY - mouseY) * WRate;
+  }
+
+  else if (holdingShift) {
+    // translate
+  }
+
+  else {
+    rotx += (pmouseY-mouseY) * rate;
+    roty += (mouseX-pmouseX) * rate;
+  }
+}
+
+void keyPressed() {
+  if (key == CODED && keyCode == SHIFT)
+    holdingShift = true;
+
+  else if (key == "w" || key == "W")
+    holdingW = true;
+}
+
+void keyReleased() {
+  if (key == CODED && keyCode == SHIFT)
+    holdingShift = false;
+
+  else if (key == "w" || key == "W")
+    holdingW = false;
+}
